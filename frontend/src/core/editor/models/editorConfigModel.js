49be871172ac815d02d7d045cb5c46103cf3f1cf
Kevin Corry <kevinc@learningpool.com>
2014-03-18 08:43:08 +0000

    Updated config model to prevent multiple fetch

--- frontend/src/core/editor/models/editorConfigModel.js
+++ frontend/src/core/editor/models/editorConfigModel.js
@@ -3,7 +3,8 @@ define(function(require) {
   var EditorModel = require('coreJS/editor/models/editorModel');
 
   var EditorConfigModel = EditorModel.extend({
-    urlRoot: '/api/content/config'
+    urlRoot: '/api/content/config',
+    initialize: function() {}
   },
   {
     _siblings:'',
